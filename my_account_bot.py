5e5a61645e51d25f11ba32e242a30b0b53323605
tinderbox <abc1238364@gmail.com>
2018-12-10 12:46:45 +0800

    Update and rename main.py to my_account_bot.py

---
+++ my_account_bot.py
@@ -0,0 +1,12 @@
+from TravianBot import Travian, AlgoMethods
+import base64
+
+username = "my_username"
+password = "password"
+
+my_account = Travian("ts2", username, password)
+# AlgoRunAs(my_account).build_by_oid([1,3,7,11,26])
+# AlgoRunAs(my_account).build_by_gid([1,2,3,4,15,15])
+AlgoRunAs(my_account).auto_run_by_min_resource()
+
+

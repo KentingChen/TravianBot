de44d1acbbb11347d01ebbd5a8b92fda5c8b6dd7
tinderbox <abc1238364@gmail.com>
2018-12-13 20:15:55 +0800

    Update my_account_bot.py

--- my_account_bot.py
+++ my_account_bot.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python
 from TravianBot import Travian, AlgoRunAs
-import time
+from pprint import pprint
+
 
 username = ""
 password = ""
 
 gid_queue_dict = {
-    "1190": [4,17,15,10,11,8,4],
-    "3144": [10,11,15,10,11,15]+[4]*15
+    "1190": "auto",
+    "3144": [10,"auto"] + ["auto"]*10
 }
 
 
-my_acct = Travian("ts2", username, password)
-AlgoRunAs(my_acct).all_build_by_gid(gid_queue_dict)
-# AlgoRunAs(my_acct).all_auto_by_min_resource()
+my_account = Travian("ts2", username, password)
+AlgoRunAs(my_account).all_build_by_gid(gid_queue_dict)
+# AlgoRunAs(my_account).all_auto_by_min_resource()

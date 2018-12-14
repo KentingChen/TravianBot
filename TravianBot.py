535bc4e935ffe4e3e70bb120567e13c41c243ead
tinderbox <abc1238364@gmail.com>
2018-12-14 11:28:12 +0800

    Update TravianBot.py

--- TravianBot.py
+++ TravianBot.py
@@ -188,9 +188,6 @@ class Travian:
             # method
             self.update_all_status()
 
-            # if current_village_link != "":
-            #    self.browser.open(current_village_link)
-
         def update_all_status(self):
             self._update_resource_level()
             # self._update_stock()
@@ -296,7 +293,6 @@ class Travian:
                         self.browser.get(self.root_url + postfix)
                         logger(f"[VILLAGE] call upgrade oid[{oid}] success")
                         self.last_upgrade_oid = oid
-                        # self.update_all_status()
                         return "green"  # upgraded success.
                     else:
                         logger("[ERROR] Multiple url list found! : " + str(url_list))
@@ -351,7 +347,6 @@ class Travian:
                     logger("[Hero] Hero is not in Village.")
                     return "unavailable"
             else:
-                # logger("[Hero] No adventure available.")
                 return "blank_list"
 
 
@@ -370,6 +365,7 @@ class AlgoRunAs:
                 for newdid in self.TRAVIAN.village_newdid_list:
                     if newdid in gid_queue_dict and bool(gid_queue_dict[newdid]) is True:
                         self.TRAVIAN.goto(newdid)
+                        # if use auto (upgrade by min stock)
                         if gid_queue_dict[newdid] == "auto" or gid_queue_dict[newdid][0] == "auto":
                             if len(self.TRAVIAN.Village.resource_status["農場"]) == 15:  # 15 田
                                 status_dict = self.TRAVIAN.Village.resource_status[gid_dict[str(4)]]
@@ -389,9 +385,9 @@ class AlgoRunAs:
                                 else:
                                     oid = min(status_dict, key=status_dict.get)
                                     last_gid = "4"
-                            else:
+                            else:  # not 15 田
                                 stock_list = self.TRAVIAN.Village.stock
-                                if stock_list[4] < 15:
+                                if stock_list[4] < 15:  # danger food balance
                                     last_gid = "4"  # farm
                                 else:
                                     last_gid = str(stock_list.index(min(stock_list[0:4])) + 1)
@@ -402,6 +398,7 @@ class AlgoRunAs:
                                         tmp_dict = dict(status_dict)
                                         tmp_dict.pop(oid)
                                         oid = min(tmp_dict, key=tmp_dict.get)
+                        # if use gid
                         else:
                             last_gid = str(gid_queue_dict[newdid][0])
                             if int(last_gid) > 4:
@@ -430,145 +427,20 @@ class AlgoRunAs:
                     self.TRAVIAN.Hero.go_adventure()
                     adventure_time = time.time()
                     time.sleep(random.randint(3, 5))
-            except ConnectionError:
+            except ConnectionError as error_message:
                 logger("[Error] Connection Error occured.")
+                logger(f"[ErrorMessage] {error_message}")
             except KeyboardInterrupt:
                 logger("[Stop] by Keyboard Interruption.")
-                if set(gid_queue_dict.values()) != {"auto"}:
-                    pprint.pprint(gid_queue_dict)
+            except TypeError as error_message:
+                logger("[Error] Type Error!")
+                logger(f"[ErrorMessage] {error_message}")
+            except ValueError as error_message:
+                logger("[Error] Value Error!")
+                logger(f"[ErrorMessage] {error_message}")
+            finally:
                 print("\n - Upgraded item list in the Last Run:")
                 pprint.pprint(upgraded_item_dict)
                 exit(0)
         logger("[DONE] All queues are empty.")
 
-    def _old_all_auto_by_min_resource(self):
-        adventure_time = time.time()
-        last_oid_dict = {}.fromkeys(self.TRAVIAN.village_newdid_list)
-        pprint_two_dicts_not_empty(self.TRAVIAN.Village.resource_status, self.TRAVIAN.Village.building_status)
-        while 1:
-            try:
-                for newdid in self.TRAVIAN.village_newdid_list:
-                    self.TRAVIAN.goto(newdid)
-                    if len(self.TRAVIAN.Village.resource_status["農場"]) == 15:  # 15 田
-                        status_dict = self.TRAVIAN.Village.resource_status[gid_dict[str(4)]]
-                        min_food_level = status_dict[min(status_dict, key=status_dict.get)]
-                        brick_level = self.TRAVIAN.Village.resource_status["泥坑"]["16"]
-                        wood_level = self.TRAVIAN.Village.resource_status["伐木場"]["3"]
-                        steel_level = self.TRAVIAN.Village.resource_status["鐵礦場"]["4"]
-                        if brick_level < min_food_level - 3:
-                            oid = "16"
-                            gid = "2"
-                        elif wood_level < min_food_level - 3:
-                            oid = "3"
-                            gid = "1"
-                        elif steel_level < min_food_level - 3:
-                            oid = "4"
-                            gid = "3"
-                        else:
-                            oid = min(status_dict, key=status_dict.get)
-                            gid = "4"
-                    else:
-                        stock_list = self.TRAVIAN.Village.stock
-                        if stock_list[4] < 15:
-                            gid = "4"  # farm
-                        else:
-                            gid = stock_list.index(min(stock_list[0:4])) + 1
-                        status_dict = self.TRAVIAN.Village.resource_status[gid_dict[str(gid)]]
-                        oid = min(status_dict, key=status_dict.get)
-                        if len(status_dict) > 1:
-                            if oid == last_oid_dict[newdid]:
-                                tmp_dict = dict(status_dict)
-                                tmp_dict.pop(oid)
-                                oid = min(tmp_dict, key=tmp_dict.get)
-                    status = self.TRAVIAN.Village.upgrade(oid)
-                    if status == "green":
-                        upgrade_item = gid_dict[str(gid)]
-                        logger(f"[UPGRADE] : [vid:{newdid}] - {upgrade_item}")
-                        last_oid_dict[newdid] = oid
-                    time.sleep(random.randint(3, 5))
-                time.sleep(random.randint(30, 60))
-                if time.time() > adventure_time + random.randint(3600, 4800):
-                    self.TRAVIAN.Hero.go_adventure()
-                    adventure_time = time.time()
-                    time.sleep(random.randint(3, 5))
-
-            except ConnectionError:
-                logger("[Error] Connection Error occured.")
-            except KeyboardInterrupt:
-                logger("[Stop] by Keyboard Interruption.")
-                exit(0)
-
-    def _old_build_by_oid(self, build_oid_queue):
-        while 1:
-            try:
-                while len(build_oid_queue) > 0:
-                    oid = build_oid_queue[0]
-                    status = self.TRAVIAN.Village.upgrade(oid)
-                    if status == "green":
-                        done_oid = build_oid_queue.pop(0)
-                        logger(f"oid({done_oid}) is building.")
-                        sec = random.randint(3, 5)
-                        time.sleep(sec)
-                    else:
-                        sec = random.randint(150, 180)
-                        logger(f"oid({oid}) skipped. Waiting {sec} seconds.")
-                        time.sleep(sec)
-                logger("build oid queue is empty now, exit...")
-                exit(0)
-            except ConnectionError:
-                logger("[ERROR] Connection Error occurred!")
-                time.sleep(random.randint(180, 200))
-                self.TRAVIAN = Travian("ts2", username, password)
-
-    def _old_build_by_gid(self, gid_queue):
-        while len(gid_queue) > 0:
-            last_gid = str(gid_queue[0])
-            self.TRAVIAN.update_all_status()
-            if int(last_gid) > 4:
-                status_dict = self.TRAVIAN.Village.building_status[gid_dict[last_gid]]
-                oid = min(status_dict, key=status_dict.get)
-            else:
-                status_dict = self.TRAVIAN.Village.resource_status[gid_dict[last_gid]]
-                oid = min(status_dict, key=status_dict.get)
-                if oid == self.TRAVIAN.Village.last_upgrade_oid:
-                    tmp_dict = dict(status_dict)
-                    tmp_dict.pop(oid)
-                    oid = min(tmp_dict, key=tmp_dict.get)
-            status = self.TRAVIAN.Village.upgrade(oid)
-            if status == "green":
-                logger(f"[UPGRADE] : {gid_dict[last_gid]}")
-                time.sleep(random.randint(10, 20))
-                gid_queue.pop(0)
-            else:
-                time.sleep(random.randint(160, 180))
-        logger("build oid queue is empty now, exit...")
-        exit(0)
-
-    def _old_auto_run_by_min_resource(self):
-        adventure_time = time.time()
-        while 1:
-            try:
-                self.TRAVIAN.update_all_status()
-                stock_list = self.TRAVIAN.Village.stock
-                if stock_list[4] < 15:
-                    gid = 4  # farm
-                else:
-                    gid = stock_list.index(min(stock_list[0:4])) + 1
-                status_dict = self.TRAVIAN.Village.resource_status[gid_dict[str(gid)]]
-                oid = min(status_dict, key=status_dict.get)
-                if len(status_dict) > 1:
-                    if oid == self.TRAVIAN.Village.last_upgrade_oid:
-                        tmp_dict = dict(status_dict)
-                        tmp_dict.pop(oid)
-                        oid = min(tmp_dict, key=tmp_dict.get)
-                status = self.TRAVIAN.Village.upgrade(oid)
-                if status == "green":
-                    time.sleep(random.randint(10, 20))
-                else:
-                    time.sleep(random.randint(160, 180))
-                if time.time() > adventure_time + 1800:
-                    self.TRAVIAN.Hero.go_adventure()
-                    adventure_time = time.time()
-            except ConnectionError:
-                logger("[ERROR] Connection Error.")
-                time.sleep(200)
